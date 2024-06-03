3e872b217188750f14e0dc2e4221a2c556f7d4af
TakaRikka <takarikka@outlook.com>
2024-06-02 22:03:16 -0700

    CARDBios done

--- include/dolphin/card/CARDBios.h
+++ include/dolphin/card/CARDBios.h
@@ -7,5 +7,10 @@ s32 CARDFreeBlocks(s32 chan, s32 *byteNotUsed, s32 *filesNotUsed);
 long CARDGetEncoding(long chan, unsigned short * encode);
 long CARDGetMemSize(long chan, unsigned short * size);
 s32 CARDGetSectorSize(s32 chan, u32 *size);
+const DVDDiskID* CARDGetDiskID(s32 chan);
+s32 CARDSetDiskID(s32 chan, const DVDDiskID* diskID);
+BOOL CARDSetFastMode(BOOL enable);
+BOOL CARDGetFastMode(void);
+s32 CARDGetCurrentMode(s32 chan, u32* mode);
 
 #endif // _DOLPHIN_CARDBIOS_H_
